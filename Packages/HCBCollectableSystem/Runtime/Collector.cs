7b90c8719680704ccfd7d8981f12f2c6fb086fc1
denoboi <deniz.kaymakci@hotmail.com>
2022-10-27 18:01:14 +0300

    Money

--- Packages/HCBCollectableSystem/Runtime/Collector.cs
+++ Packages/HCBCollectableSystem/Runtime/Collector.cs
@@ -11,7 +11,7 @@ namespace HCB.CollectableSystem
         public bool CanCollect { get => _canCollect; protected set => _canCollect = value; }
         private void OnTriggerEnter(Collider other)
         {
-            ICollectable collectable = other.GetComponentInChildren<ICollectable>();
+            ICollectable collectable = other.GetComponentInParent<ICollectable>();
             if (collectable != null)
             {
                 collectable.Collect(this);
@@ -21,7 +21,7 @@ namespace HCB.CollectableSystem
 
         private void OnCollisionEnter(Collision collision)
         {
-            ICollectable collectable = collision.collider.GetComponentInChildren<ICollectable>();
+            ICollectable collectable = collision.collider.GetComponentInParent<ICollectable>();
             if (collectable != null)
             {
                 collectable.Collect(this);
